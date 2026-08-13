147d756de5b9d752e417e4f639496a9bc3b52567
Joshua James <joshuajames.o@me.com>
2026-08-13 13:22:06 +0200

    Initial Commit

---
+++ Meal PlannerTests/Meal_PlannerTests.swift
@@ -0,0 +1,19 @@
+//
+//  Meal_PlannerTests.swift
+//  Meal PlannerTests
+//
+//  Created by Joshua James O’Connor on 13/08/2026.
+//
+
+import Testing
+@testable import Meal_Planner
+
+struct Meal_PlannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
